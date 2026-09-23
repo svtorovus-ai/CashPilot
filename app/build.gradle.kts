b30331d945fa04d227b81b25461c658d4fb26ee4
Grey <s.v.torovus@gmail.com>
2026-09-23 21:14:39 +0300

    Bump CashPilot for settings guide update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "ua.cashpilot"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
     }
 
     signingConfigs {
